7d23d4671fce4dbdfb44439126b1c6d0f34ef55d
Enimhien <akhasfaith@gmail.com>
2024-09-03 17:34:34 +0100

    added analysis of new set of data

--- TRYdata_analysis.R
+++ TRYdata_analysis.R
@@ -325,3 +325,252 @@ rtry_export(num_traits_georef_wider2, output_file)
 # Export the data into a CSV file
 output_file = file.path(tempdir(), "workdata_traits2.csv")
 rtry_export(num_traits_georef2, output_file)
+
+
+
+
+
+
+
+
+#----------------------------------------- new data added
+#----------do well to check that the values are appropriately defined e.g workdata3. num_trait3, TRYdata6 etc---------
+
+
+# Marijn sent some TRY data, I have carried out some analysis to process the data----
+
+
+# Importing and exploring dataset ----
+## For TRYdata1 ----
+TRYdata3 <- rtry_import("~/OneDrive - University of Exeter/Desktop/O3-/TRY DataFile/35747.txt")
+View(TRYdata3)
+head(TRYdata3)
+
+TRYdata4 <- rtry_import("~/OneDrive - University of Exeter/Desktop/O3-/TRY DataFile/35746.txt")
+
+TRYdata5 <- rtry_import("~/OneDrive - University of Exeter/Desktop/O3-/TRY DataFile/35745.txt")
+
+## Group the input data based on TraitID and TraitName ----
+TRYdata3_explore_trait <- rtry_explore(TRYdata3, TraitID, TraitName)
+View(TRYdata3_explore_trait)
+
+
+## Group the input data based on AccSpeciesID, AccSpeciesName, TraitID and TraitName----
+## Note: For TraitID == "NA", meaning that entry is an ancillary data----
+TRYdata3_explore_species <- rtry_explore(TRYdata3, AccSpeciesID, AccSpeciesName, TraitID, TraitName)
+View(TRYdata3_explore_species)
+
+
+## Group the input data based on DataID, DataName, TraitID and TraitName----
+## Then sort the output by TraitID using the sortBy argument----
+TRYdata3_explore_anc <- rtry_explore(TRYdata3, DataID, DataName, TraitID, TraitName, sortBy = TraitID)
+View(TRYdata3_explore_anc)
+
+
+
+
+
+
+
+
+
+## Group the input data based on TraitID and TraitName ----
+TRYdata4_explore_trait <- rtry_explore(TRYdata4, TraitID, TraitName)
+View(TRYdata4_explore_trait)
+
+
+## Group the input data based on AccSpeciesID, AccSpeciesName, TraitID and TraitName----
+## Note: For TraitID == "NA", meaning that entry is an ancillary data----
+TRYdata4_explore_species <- rtry_explore(TRYdata4, AccSpeciesID, AccSpeciesName, TraitID, TraitName)
+View(TRYdata4_explore_species)
+
+
+## Group the input data based on DataID, DataName, TraitID and TraitName----
+## Then sort the output by TraitID using the sortBy argument----
+TRYdata4_explore_anc <- rtry_explore(TRYdata4, DataID, DataName, TraitID, TraitName, sortBy = TraitID)
+View(TRYdata4_explore_anc)
+
+
+
+
+
+
+
+
+## Group the input data based on TraitID and TraitName ----
+TRYdata5_explore_trait <- rtry_explore(TRYdata5, TraitID, TraitName)
+View(TRYdata5_explore_trait)
+
+
+## Group the input data based on AccSpeciesID, AccSpeciesName, TraitID and TraitName----
+## Note: For TraitID == "NA", meaning that entry is an ancillary data----
+TRYdata5_explore_species <- rtry_explore(TRYdata5, AccSpeciesID, AccSpeciesName, TraitID, TraitName)
+View(TRYdata5_explore_species)
+
+
+## Group the input data based on DataID, DataName, TraitID and TraitName----
+## Then sort the output by TraitID using the sortBy argument----
+TRYdata5_explore_anc <- rtry_explore(TRYdata5, DataID, DataName, TraitID, TraitName, sortBy = TraitID)
+View(TRYdata5_explore_anc)
+
+
+
+
+TRYdata6 <- rtry_bind_row(TRYdata3, TRYdata4, TRYdata5)
+
+
+# Group the input data based on TraitID and TraitName
+TRYdata6_explore_trait <- rtry_explore(TRYdata6, TraitID, TraitName)
+
+# Group the input data based on AccSpeciesID, AccSpeciesName, TraitID and TraitName
+# Note: For TraitID == "NA", meaning that entry is an ancillary data
+TRYdata6_explore_species <- rtry_explore(TRYdata6, AccSpeciesID, AccSpeciesName, TraitID, TraitName)
+
+# Group the input data based on DataID, DataName, TraitID and TraitName
+# Then sort the output by TraitID using the sortBy argument
+TRYdata6_explore_anc <- rtry_explore(TRYdata6, DataID, DataName, TraitID, TraitName, sortBy = TraitID)
+
+
+
+
+
+
+
+
+
+
+
+# Keeping columns and rows ----
+
+workdata3 <- rtry_remove_col(TRYdata6, V29)
+
+
+workdata3 <- rtry_select_col(TRYdata6, ObsDataID, ObservationID, AccSpeciesID, AccSpeciesName, ValueKindName, TraitID, TraitName, DataID, DataName, OriglName, OrigValueStr, OrigUnitStr, StdValue, UnitName, OrigObsDataID, ErrorRisk, Comment)
+View(workdata)
+
+workdata3_explore_anc <- rtry_explore(workdata3, DataID, DataName, TraitID, TraitName, sortBy = TraitID)
+View(workdata3_explore_anc)
+
+
+workdata <- rtry_select_row(workdata, TraitID > 0 | DataID %in% c(59, 60, 61, 6601, 327, 413, 1961, 113))
+View(workdata)
+
+workdata3_explore_anc <- rtry_explore(workdata3, DataID, DataName, TraitID, TraitName, sortBy = TraitID)
+View(workdata3_explore_anc)
+
+
+# It is recommended to backup the data at some stages of the preprocessing, best before excluding according to different attributes.
+
+workdata3_unexcluded <- workdata3
+
+
+# Georeference coding to exclude data with only lat and long ----
+## Filter according to latitude ----
+## First, obtain only the observations that contain the Latitude (DataID 59) information, i.e. geo-referenced observations, using the function rtry_select_row.----
+# Select only the geo-referenced observations, i.e. with DataID 59 Latitude
+# Set getAncillary to TRUE to obtain (keep) all traits and ancillary data
+
+workdata3 <- rtry_select_row(workdata3, DataID %in% 59, getAncillary = TRUE)
+
+# Select the rows that contain DataID 59, i.e. latitude information
+# Then explore the unique values of the StdValue within the selected data
+
+tmp_unfiltered3 <- rtry_select_row(workdata3, DataID %in% 59)
+tmp_unfiltered3 <- rtry_explore(tmp_unfiltered3, DataID, DataName, OriglName, OrigValueStr, OrigUnitStr, StdValue, Comment, sortBy = StdValue)
+View(tmp_unfiltered3)
+
+# # # # # # # # # # # # #
+# Exclude observations using latitude information
+# Criteria
+# 1. DataID equals to 59
+# 2. StdValue NA
+
+workdata3 <- rtry_exclude(workdata3, (DataID %in% 59) & (is.na(StdValue)), baseOn = ObservationID)
+View(workdata3)
+
+# Select the rows where DataID is 59 (Latitude)
+# Then explore the unique values of the StdValue within the selected data
+# Sort the exploration by StdValue
+
+tmp_filtered3 <- rtry_select_row(workdata3, DataID %in% 59)
+tmp_filtered3 <- rtry_explore(tmp_filtered3, DataID, DataName, OriglName, OrigValueStr, OrigUnitStr, StdValue, Comment, sortBy = StdValue)
+View(tmp_filtered3)
+
+
+#Filter according to longitude
+#A similar procedure will be performed for longitude (DataID 60). To ensure the all the observations within the workdata contains the longitude information, use the rtry_select_row function.
+
+
+
+# Select only the geo-referenced observations with DataID 60 Longitude
+# Set getAncillary to TRUE to obtain (keep) all traits and ancillary data
+workdata3 <- rtry_select_row(workdata3, DataID %in% 60, getAncillary = TRUE)
+
+
+# Select the rows that contain DataID 60, i.e. longitude information
+# Then explore the unique values of the StdValue within the selected data
+tmp_unfiltered3 <- rtry_select_row(workdata3, DataID %in% 60)
+tmp_unfiltered3 <- rtry_explore(tmp_unfiltered3, DataID, DataName, OriglName, OrigValueStr, OrigUnitStr, StdValue, Comment, sortBy = StdValue)
+View(tmp_unfiltered3)
+
+
+# Exclude observations using longitude information
+# Criteria
+# 1. DataID equals to 60
+# 2. StdValue smaller than 10 or larger than 60 or NA
+workdata3 <- rtry_exclude(workdata3, (DataID %in% 60) & (is.na(StdValue)), baseOn = ObservationID)
+
+# Select the rows where DataID is 60 (Longitude)
+# Then explore the unique values of the StdValue within the selected data
+# Sort the exploration by StdValue
+tmp_filtered3 <- rtry_select_row(workdata3, DataID %in% 60)
+tmp_filtered3 <- rtry_explore(tmp_filtered3, DataID, DataName, OriglName, OrigValueStr, OrigUnitStr, StdValue, Comment, sortBy = StdValue)
+View(tmp_filtered3)
+
+
+
+# Remove duplicates
+workdata3 <- rtry_remove_dup(workdata3)
+
+
+#transform to wide table
+# Exclude
+# 1. All entries with "" in TraitID
+# 2. Potential categorical traits that don't have a StdValue
+# 3. Traits that have not yet been standardized in TRY
+# Then select the relevant columns for transformation
+# Note: The complete.cases() is used to ensure the cases are complete,
+#       i.e. have no missing values
+num_traits3 <- rtry_select_row(workdata3, complete.cases(TraitID) & complete.cases(StdValue))
+
+num_traits3 <- rtry_select_col(num_traits3, ObservationID, AccSpeciesID, AccSpeciesName, TraitID, TraitName, StdValue, UnitName)
+
+# Extract the unique value of latitude (DataID 59) and longitude (DataID 60) together with the corresponding ObservationID
+workdata_georef3 <- rtry_select_anc(workdata3, 59, 60)
+
+# To merge the extracted ancillary data with the numerical traits
+# Merge the relevant data frames based on the ObservationID using rtry_join_left (left join)
+num_traits_georef3 <- rtry_join_left(num_traits3, workdata_georef3, baseOn = ObservationID)
+
+View(num_traits_georef3)
+
+
+# Perform wide table transformation on TraitID, TraitName and UnitName
+# With cell values to be the mean values calculated for StdValue
+num_traits_georef_wider3 <- rtry_trans_wider(num_traits_georef3, names_from = c(TraitID, TraitName, UnitName), values_from = c(StdValue), values_fn = list(StdValue = mean))
+View(num_traits_georef_wider3)
+
+
+#export data
+
+# Export the data into a CSV file
+output_file = file.path(tempdir(), "workdata_wider_traits_Marijn.csv")
+rtry_export(num_traits_georef_wider3, output_file)
+
+
+
+# Export the data into a CSV file
+output_file = file.path(tempdir(), "workdata_traits_Marijn.csv")
+rtry_export(num_traits_georef3, output_file)
+
+
